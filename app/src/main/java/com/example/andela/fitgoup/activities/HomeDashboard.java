7841895b3133c97423b32fc47afe6760a1b94744
andela <chidiebere.nwokocha@andela.com>
2016-03-08 00:20:42 +0100

    [#115053925] Add tab icons

--- app/src/main/java/com/example/andela/fitgoup/activities/HomeDashboard.java
+++ app/src/main/java/com/example/andela/fitgoup/activities/HomeDashboard.java
@@ -11,16 +11,23 @@ import android.support.v4.view.ViewPager;
 import android.os.Bundle;
 
 import com.example.andela.fitgoup.R;
+import com.example.andela.fitgoup.fragments.CalendarFragment;
 import com.example.andela.fitgoup.fragments.ExerciseFragment;
 import com.example.andela.fitgoup.fragments.StatisticsFragment;
 import com.example.andela.fitgoup.fragments.SettingsFragment;
-import com.example.andela.fitgoup.fragments.CalendarFragment;
+import com.example.andela.fitgoup.fragments.InfoFragment;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class HomeDashboard extends AppCompatActivity {
   private ViewPager mViewPager;
+  private TabLayout tabs;
+  private int[] tabIcons = {R.drawable.ic_fitness_center,
+      R.drawable.ic_help_outline,
+      R.drawable.ic_settings_white,
+      R.drawable.ic_show_chart,
+      R.drawable.ic_calendar};
 
   public HomeDashboard() {}
 
@@ -29,20 +36,34 @@ public class HomeDashboard extends AppCompatActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_home_dashboard);
     Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-    setTitle("Fitgoup");
+    setTitle("PushFit");
     setSupportActionBar(toolbar);
     mViewPager = (ViewPager) findViewById(R.id.container);
     setUpPager(mViewPager);
-    TabLayout tabs = (TabLayout) findViewById(R.id.tabs);
+    tabs = (TabLayout) findViewById(R.id.tabs);
     tabs.setupWithViewPager(mViewPager);
+    setTabIcons();
+  }
+
+  public void setTabIcons() {
+    try {
+      tabs.getTabAt(0).setIcon(tabIcons[0]);
+      tabs.getTabAt(1).setIcon(tabIcons[2]);
+      tabs.getTabAt(2).setIcon(tabIcons[1]);
+      tabs.getTabAt(3).setIcon(tabIcons[3]);
+      tabs.getTabAt(4).setIcon(tabIcons[4]);
+    } catch (NullPointerException npe) {
+      npe.getCause().printStackTrace();
+    }
   }
 
   public void setUpPager(ViewPager viewPager) {
     SectionsPagerAdapter adapter = new SectionsPagerAdapter(getSupportFragmentManager());
     adapter.addPageFragment(new ExerciseFragment(), "Exercise");
     adapter.addPageFragment(new SettingsFragment(), "Settings");
-    adapter.addPageFragment(new CalendarFragment(), "Calendar");
+    adapter.addPageFragment(new InfoFragment(), "Info");
     adapter.addPageFragment(new StatisticsFragment(), "Statistics");
+    adapter.addPageFragment(new CalendarFragment(), "Calendar");
     viewPager.setAdapter(adapter);
   }
 
--- app/src/main/java/com/example/andela/fitgoup/fragments/ExerciseFragment.java
+++ app/src/main/java/com/example/andela/fitgoup/fragments/ExerciseFragment.java
@@ -5,24 +5,38 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.example.andela.fitgoup.R;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 /**
  * Created by andela on 3/6/16.
  */
 public class ExerciseFragment extends Fragment {
 
-  public void FirstFragment() {}
+  public ExerciseFragment() {}
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
+
     super.onCreate(savedInstanceState);
+
   }
 
   @Override
   public View onCreateView(LayoutInflater layoutInflater, ViewGroup group, Bundle savedInstance) {
     return layoutInflater.inflate(R.layout.fragment_home_dashboard, group, false);
+  }
+
+  @Override
+  public void onViewCreated(View view, Bundle savedInstance) {
+    TextView dateTextview = (TextView) view.findViewById(R.id.fragment_exercise);
+    GregorianCalendar currentCalendar = new GregorianCalendar();
+    dateTextview.setText(String.valueOf(currentCalendar.get(Calendar.DAY_OF_WEEK)));
   }
 
 }
