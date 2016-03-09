622876465ae0649675c05595c2fa60746e0dd1c2
andela <chidiebere.nwokocha@andela.com>
2016-03-09 19:02:02 +0100

    [Finish #115237591] Allow user save exercise statistics

--- app/src/main/java/com/example/andela/fitgoup/model/PushUpModel.java
+++ app/src/main/java/com/example/andela/fitgoup/model/PushUpModel.java
@@ -1,7 +1,36 @@
 package com.example.andela.fitgoup.model;
 
+import android.database.Cursor;
+
+import com.activeandroid.Cache;
+import com.activeandroid.Model;
+import com.activeandroid.annotation.Column;
+import com.activeandroid.annotation.Table;
+import com.activeandroid.query.Select;
+
+import java.util.Date;
+import java.util.List;
+
 /**
- * Created by andela on 3/8/16.
+ * Created by Chidi on 3/8/16.
  */
-public class PushUpModel {
+
+@Table(name = "PushUp")
+public class PushUpModel extends Model {
+
+  @Column(name = "day_pushup")
+  public int pushups;
+
+  public PushUpModel () {
+    super();
+  }
+
+  public PushUpModel(int pushup) {
+    super();
+    this.pushups = pushup;
+  }
+
+  public static List<PushUpModel> fetchPushups() {
+    return new Select().all().from(PushUpModel.class).execute();
+  }
 }
