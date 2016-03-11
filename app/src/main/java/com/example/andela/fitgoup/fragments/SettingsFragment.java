74317327a040bbe2b4cbb4f69a0bd529f50d4641
andela <chidiebere.nwokocha@andela.com>
2016-03-11 09:27:16 +0100

    [#115236991] Add Settings configuration to the SettingsFragment

--- app/src/main/java/com/example/andela/fitgoup/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/andela/fitgoup/fragments/SettingsFragment.java
@@ -1,19 +1,26 @@
 package com.example.andela.fitgoup.fragments;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
+import android.preference.EditTextPreference;
+import android.preference.PreferenceManager;
+import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.SwitchPreferenceCompat;
+import android.text.InputType;
+import android.util.Log;
 
 import com.example.andela.fitgoup.R;
 
 /**
  * Created by andela on 3/6/16.
  */
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends PreferenceFragmentCompat {
 
-  public SettingsFragment() {}
+  private SwitchPreferenceCompat countdown;
+  private SwitchPreferenceCompat timer;
+  private SharedPreferences.OnSharedPreferenceChangeListener listener;
+  private SharedPreferences sharedPreferences;
+
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -21,7 +28,42 @@ public class SettingsFragment extends Fragment {
   }
 
   @Override
-  public View onCreateView(LayoutInflater layoutInflater, ViewGroup group, Bundle savedInstance) {
-    return layoutInflater.inflate(R.layout.fragment_two, group, false);
+  public void onCreatePreferences(Bundle bundle, String s) {
+    addPreferencesFromResource(R.xml.pref_setting);
+    sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+
+    listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+      @Override
+      public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        timer = (SwitchPreferenceCompat) findPreference("timer_switch");
+        countdown = (SwitchPreferenceCompat) findPreference("pushup_switch");
+        if((key.equals("timer_switch") || key.equals("timer_count")) && timer.isChecked()) {
+          countdown.setChecked(false);
+          timer.setSummary(String.valueOf(sharedPreferences.getBoolean("timer_switch", false)));
+          countdown.setSummary(String.valueOf(sharedPreferences.getBoolean("pushup_switch", false)));
+          findPreference("timer_count").setSummary(sharedPreferences.getString("timer_count", "5"));
+
+        } else if (key.equals("pushup_switch") && countdown.isChecked()) {
+          timer.setChecked(false);
+          Log.i("switches", "" + timer.isChecked());
+          timer.setSummary(String.valueOf(sharedPreferences.getBoolean("timer_switch", false)));
+          countdown.setSummary(String.valueOf(sharedPreferences.getBoolean("pushup_switch", false)));
+          findPreference("pushup_count").setSummary(String.valueOf(sharedPreferences.getString("pushup_count", "10")));
+        }
+      }
+    };
+    getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(listener);
+  }
+
+  @Override
+  public void onResume() {
+    super.onResume();
+    getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(listener);
+  }
+
+  @Override
+  public void onPause() {
+    super.onPause();
+    getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(listener);
   }
 }
