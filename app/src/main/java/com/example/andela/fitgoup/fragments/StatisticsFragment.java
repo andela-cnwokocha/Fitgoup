adbcabf5b3384eb22d44b2a646003b93308965ef
andela <chidiebere.nwokocha@andela.com>
2016-03-15 18:22:36 +0100

    [#115560435] Customize layout without Saved data

--- app/src/main/java/com/example/andela/fitgoup/fragments/StatisticsFragment.java
+++ app/src/main/java/com/example/andela/fitgoup/fragments/StatisticsFragment.java
@@ -7,6 +7,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.example.andela.fitgoup.R;
 import com.example.andela.fitgoup.model.PushUpModel;
@@ -34,7 +35,7 @@ public class StatisticsFragment extends Fragment {
 
   @Override
   public void onViewCreated(View view, Bundle savedInstance) {
-    ValueLineChart mCubicValueLineChart = (ValueLineChart) view.findViewById(R.id.cubiclinechart);
+    ValueLineChart mCubicValueLineChart = (ValueLineChart) view.findViewById(R.id.linechart);
 
     ValueLineSeries series = new ValueLineSeries();
     series.setColor(0xFF56B7F1);
@@ -59,23 +60,10 @@ public class StatisticsFragment extends Fragment {
       for (PushUpModel pushUpModel: pushuprecords) {
         series.addPoint(new ValueLinePoint(pushUpModel.currentDay, (float)pushUpModel.pushups));
       }
+    } else {
+      LinearLayout layout = (LinearLayout) view.findViewById(R.id.noNoteLayout);
+      layout.setVisibility(View.VISIBLE);
     }
-/*
-
-    series.addPoint(new ValueLinePoint("Jan", 2.4f));
-    series.addPoint(new ValueLinePoint("Feb", 3.4f));
-    series.addPoint(new ValueLinePoint("Mar", .4f));
-    series.addPoint(new ValueLinePoint("Feb", 1.2f));
-    series.addPoint(new ValueLinePoint("Mai", 2.6f));
-    series.addPoint(new ValueLinePoint("Jun", 1.0f));
-    series.addPoint(new ValueLinePoint("Jul", 3.5f));
-    series.addPoint(new ValueLinePoint("Aug", 2.4f));
-    series.addPoint(new ValueLinePoint("Sep", 2.4f));
-    series.addPoint(new ValueLinePoint("Oct", 3.4f));
-    series.addPoint(new ValueLinePoint("Nov", .4f));
-    series.addPoint(new ValueLinePoint("Dec", 1.3f));
-*/
-
     mCubicValueLineChart.addSeries(series);
     mCubicValueLineChart.startAnimation();
   }
