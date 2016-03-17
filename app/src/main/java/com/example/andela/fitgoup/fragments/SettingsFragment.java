0ca36977beeb1db76e2ada121a040265c06464f8
andela <chidiebere.nwokocha@andela.com>
2016-03-17 17:42:53 +0100

    [#115856155] Make changes to Settings preference to use NumberPickers for numbers

--- app/src/main/java/com/example/andela/fitgoup/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/andela/fitgoup/fragments/SettingsFragment.java
@@ -3,7 +3,7 @@ package com.example.andela.fitgoup.fragments;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
-import android.support.v7.preference.PreferenceFragmentCompat;
+import me.philio.preferencecompatextended.PreferenceFragmentCompat;
 import android.support.v7.preference.SwitchPreferenceCompat;
 
 import com.example.andela.fitgoup.R;
@@ -17,13 +17,6 @@ public class SettingsFragment extends PreferenceFragmentCompat {
   private SharedPreferences.OnSharedPreferenceChangeListener listener;
   private SharedPreferences sharedPreferences;
 
-
-  @Override
-  public void onCreate(Bundle savedInstanceState) {
-    super.onCreate(savedInstanceState);
-    getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(listener);
-  }
-
   @Override
   public void onCreatePreferences(Bundle bundle, String s) {
     addPreferencesFromResource(R.xml.pref_setting);
@@ -34,17 +27,14 @@ public class SettingsFragment extends PreferenceFragmentCompat {
       public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         timer = (SwitchPreferenceCompat) findPreference("timer_switch");
         countdown = (SwitchPreferenceCompat) findPreference("pushup_switch");
-        if((key.equals("timer_switch") || key.equals("timer_count")) && timer.isChecked()) {
+        if((key.equals("timer_switch")) && timer.isChecked()) {
           countdown.setChecked(false);
           timer.setSummary(String.valueOf(sharedPreferences.getBoolean("timer_switch", false)));
           countdown.setSummary(String.valueOf(sharedPreferences.getBoolean("pushup_switch", false)));
-          findPreference("timer_count").setSummary(sharedPreferences.getString("timer_count", "5"));
-
-        } else if ((key.equals("pushup_switch") || key.equals("pushup_count")) && countdown.isChecked()) {
+        } else if (key.equals("pushup_switch") && countdown.isChecked()) {
           timer.setChecked(false);
           countdown.setSummary(String.valueOf(sharedPreferences.getBoolean("pushup_switch", false)));
           timer.setSummary(String.valueOf(sharedPreferences.getBoolean("timer_switch", false)));
-          findPreference("pushup_count").setSummary(String.valueOf(sharedPreferences.getString("pushup_count", "10")));
         }
       }
     };
@@ -55,16 +45,12 @@ public class SettingsFragment extends PreferenceFragmentCompat {
   public void onResume() {
     super.onResume();
     getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(listener);
-    findPreference("timer_count").setSummary(sharedPreferences.getString("timer_count", "5"));
-    findPreference("pushup_count").setSummary(String.valueOf(sharedPreferences.getString("pushup_count", "10")));
   }
 
   @Override
   public void onPause() {
     super.onPause();
     getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(listener);
-    findPreference("timer_count").setSummary(sharedPreferences.getString("timer_count", "5"));
-    findPreference("pushup_count").setSummary(String.valueOf(sharedPreferences.getString("pushup_count", "10")));
   }
 
 }
--- app/src/main/java/com/example/andela/fitgoup/utils/NumberPickerPreference.java
+++ app/src/main/java/com/example/andela/fitgoup/utils/NumberPickerPreference.java
@@ -1,4 +1,5 @@
-package com.example.andela.fitgoup.utils;
+/*package com.example.andela.fitgoup.utils;
+
 
 import android.content.Context;
 import android.content.res.TypedArray;
@@ -85,5 +86,123 @@ public class NumberPickerPreference extends DialogPreference {
 
     lastHour=getHour(time);
     lastMinute=getMinute(time);
+  }
+}
+
+<com.example.andela.fitgoup.utils.NumberPickerPreference
+        android:key="timer_count"
+        android:dependency="timer_switch"
+        android:title="Minutes"/>
+
+        <!--<EditTextPreference
+            android:key="timer_count"
+            android:dependency="timer_switch"
+            android:layout="?android:attr/preferenceLayoutChild"
+            android:title="Minutes"
+            android:summary="Rate number of minutes to pushup"
+            android:defaultValue="5"
+            android:dialogTitle="Enter the number of minutes you will do pushups" />-->
+*/
+
+
+package com.example.andela.fitgoup.utils;
+
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.content.res.TypedArray;
+import android.preference.DialogPreference;
+
+//import android.preference.Preference;
+import android.preference.PreferenceManager;
+import android.support.v7.preference.Preference;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.NumberPicker;
+
+import com.example.andela.fitgoup.R;
+
+
+
+public class NumberPickerPreference extends DialogPreference implements  NumberPicker.OnValueChangeListener {
+  private static final String NUMBER_PICKER_DEFAULT = "10";
+  private NumberPicker numberPicker;
+  private SharedPreferences sharedPreferences;
+  private String savedValue;
+
+  public NumberPickerPreference(Context context, AttributeSet attrs) {
+    super(context, attrs);
+    setDialogLayoutResource(R.layout.number_preference);
+    //setOnPreferenceChangeListener(this);
+
+  }
+
+  @Override
+  protected void onDialogClosed(boolean positiveResult) {
+    super.onDialogClosed(positiveResult);
+    int pno = numberPicker.getValue();
+    if (positiveResult) {
+      String push_number = String.valueOf(pno);
+      if (callChangeListener(push_number)) {
+        saveString("push_count", push_number);
+      }
+    }
+    setSummary(pno + " Push Ups");
+  }
+
+  @Override
+  protected void onSetInitialValue(boolean restorePersistedValue, Object defaultValue) {
+    if (restorePersistedValue) {
+      setSummary(loadString() + " Push Ups");
+    } else {
+      setSummary(defaultValue.toString() + " Push Ups");
+      persistString(defaultValue.toString());
+    }
+  }
+
+  /*@Override
+  public boolean onPreferenceChange(Preference preference, Object newValue) {
+    setSummary(newValue.toString() + " Push Ups");
+    saveString("push_count", newValue.toString());
+    return false;
+  }*/
+
+  @Override
+  public void onValueChange(NumberPicker picker, int oldVal, int newVal) {
+
+  }
+
+  public void setTitle(CharSequence title) {
+    super.setTitle(title);
+  }
+
+  protected Object onGetDefaultValue(TypedArray array, int index) {
+    return array.getString(index);
+  }
+
+  protected View onCreateDialogView() {
+    View view = super.onCreateDialogView();
+    numberPicker = (NumberPicker) view.findViewById(R.id.number_picker);
+    numberPicker.setMaxValue(300);
+    numberPicker.setMinValue(5);
+    numberPicker.setValue(Integer.parseInt(loadString()));
+    return view;
+  }
+
+  protected void onBindDialogView(View view) {
+    super.onBindDialogView(view);
+  }
+
+  public void saveString(String key, String value) {
+    sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+    SharedPreferences.Editor editor = sharedPreferences.edit();
+    editor.putString(key, value);
+    editor.apply();
+  }
+
+  public String loadString() {
+    sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getContext());
+    savedValue = sharedPreferences.getString("push_count", NUMBER_PICKER_DEFAULT);
+    return savedValue;
   }
 }
