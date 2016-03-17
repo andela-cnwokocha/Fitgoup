bce433708666addfef0e0b10c8780684a5256594
andela <chidiebere.nwokocha@andela.com>
2016-03-17 18:29:24 +0100

    [#115856155] Allow a user select the rate at which they train

--- app/src/main/java/com/example/andela/fitgoup/fragments/SettingsFragment.java
+++ app/src/main/java/com/example/andela/fitgoup/fragments/SettingsFragment.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.preference.PreferenceManager;
 import me.philio.preferencecompatextended.PreferenceFragmentCompat;
 import android.support.v7.preference.SwitchPreferenceCompat;
+import android.util.Log;
 
 import com.example.andela.fitgoup.R;
 /**
@@ -16,6 +17,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
   private SwitchPreferenceCompat timer;
   private SharedPreferences.OnSharedPreferenceChangeListener listener;
   private SharedPreferences sharedPreferences;
+  private SwitchPreferenceCompat scheduleTime;
 
   @Override
   public void onCreatePreferences(Bundle bundle, String s) {
@@ -27,6 +29,7 @@ public class SettingsFragment extends PreferenceFragmentCompat {
       public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
         timer = (SwitchPreferenceCompat) findPreference("timer_switch");
         countdown = (SwitchPreferenceCompat) findPreference("pushup_switch");
+        scheduleTime = (SwitchPreferenceCompat) findPreference("pushup_time");
         if((key.equals("timer_switch")) && timer.isChecked()) {
           countdown.setChecked(false);
           timer.setSummary(String.valueOf(sharedPreferences.getBoolean("timer_switch", false)));
@@ -35,6 +38,10 @@ public class SettingsFragment extends PreferenceFragmentCompat {
           timer.setChecked(false);
           countdown.setSummary(String.valueOf(sharedPreferences.getBoolean("pushup_switch", false)));
           timer.setSummary(String.valueOf(sharedPreferences.getBoolean("timer_switch", false)));
+        } else if(key.equals("pushup_day") && scheduleTime.isChecked()) {
+          sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+          int val = sharedPreferences.getInt("pushup_day", 3);
+          findPreference("pushup_day").setTitle(String.format("Remind me every %d day(s)",val));
         }
       }
     };
