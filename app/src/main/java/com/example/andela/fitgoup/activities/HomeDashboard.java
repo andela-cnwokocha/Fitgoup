eca749b199a3d2fbe38302788aed86bce9316d27
andela <chidiebere.nwokocha@andela.com>
2016-03-21 15:12:28 +0100

    [#116038947]

--- app/src/main/java/com/example/andela/fitgoup/activities/HomeDashboard.java
+++ app/src/main/java/com/example/andela/fitgoup/activities/HomeDashboard.java
@@ -11,13 +11,10 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.os.Bundle;
-import android.util.Log;
 
 import com.beardedhen.androidbootstrap.TypefaceProvider;
 import com.example.andela.fitgoup.R;
