25fd09a8f8dcc4c0fc38158499d74025cea52101
andela-cnwokocha <chidiebere.nwokocha@andela.com>
2016-03-23 06:39:59 +0100

    [#115994971] Display intro tab once

--- app/src/main/java/com/example/andela/fitgoup/activities/HomeDashboard.java
+++ app/src/main/java/com/example/andela/fitgoup/activities/HomeDashboard.java
@@ -43,32 +43,18 @@ public class HomeDashboard extends AppCompatActivity {
     Thread t = new Thread(new Runnable() {
       @Override
       public void run() {
-        //  Initialize SharedPreferences
         SharedPreferences getPrefs = PreferenceManager
             .getDefaultSharedPreferences(getBaseContext());
-
-        //  Create a new boolean and preference and set it to true
         boolean isFirstStart = getPrefs.getBoolean("firstStart", true);
-
-        //  If the activity has never started before...
         if (isFirstStart) {
-
-          //  Launch app intro
           Intent i = new Intent(HomeDashboard.this, AppIntroActivity.class);
           startActivity(i);
-
-          //  Make a new preferences editor
           SharedPreferences.Editor e = getPrefs.edit();
-
-          //  Edit preference to make it false because we don't want this to run again
           e.putBoolean("firstStart", false);
-
-          //  Apply changes
           e.apply();
         }
       }
     });
-    // Start the thread
     t.start();
 
     TypefaceProvider.registerDefaultIconSets();
